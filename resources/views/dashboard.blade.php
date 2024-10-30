4aac679adb7bf51c02101312ca39448e944faaa3
ixacrazka <mrazfaudzan@gmail.com>
2024-10-30 14:39:00 +0700

    Tidy Up code

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,6 +1,8 @@
 @extends('layouts.app')
 
 @section('content')
+
+ <!-- Aleert -->
 <div class="flex">
     <div class="py-12">
         <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
@@ -9,105 +11,109 @@
                     {{ session('success') }}
                 </div>
             @endif
+ <!-- Aleert -->
 
+            <!--TABEL PELAPORAN  -->
             <div class="overflow-hidden">
                 <div class="p-6 text-black">
-                        <table id="pelaporTable" class="min-w-full overflow-hidden rounded-3xl shadow-lg">
-                            <thead class="bg-gray-200">
+                    <table id="pelaporTable" class="min-w-full rounded-3xl shadow-lg overflow-hidden">
+                        <thead class="bg-gray-200">
+                            <tr>
+                                <th class="px-6 py-3 text-sm font-medium tracking-wider text-left text-gray-700 uppercase">Email</th>
+                                <th class="px-6 py-3 text-sm font-medium tracking-wider text-left text-gray-700 uppercase">No. HP</th>
+                                <th class="px-6 py-3 text-sm font-medium tracking-wider text-left text-gray-700 uppercase">Nama Pelapor</th>
+                                <th class="px-6 py-3 text-sm font-medium tracking-wider text-left text-gray-700 uppercase">Nama Aplikasi</th>
+                                <th class="px-6 py-3 text-sm font-medium tracking-wider text-left text-gray-700 uppercase">Laporan Error</th>
+                                <th class="px-6 py-3 text-sm font-medium tracking-wider text-left text-gray-700 uppercase">Status</th>
+                                <th class="px-6 py-3 text-sm font-medium tracking-wider text-left text-gray-700 uppercase">Foto Error</th>
+                                <th class="px-6 py-3 text-sm font-medium tracking-wider text-left text-gray-700 uppercase">Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody class="bg-white divide-y divide-gray-200">
+                            @foreach($pelapors as $pelapor)
                                 <tr>
-                                    <th class="px-6 py-3 text-sm font-medium tracking-wider text-left text-gray-700 uppercase">Email</th>
-                                    <th class="px-6 py-3 text-sm font-medium tracking-wider text-left text-gray-700 uppercase">No. HP</th>
-                                    <th class="px-6 py-3 text-sm font-medium tracking-wider text-left text-gray-700 uppercase">Nama Pelapor</th>
-                                    <th class="px-6 py-3 text-sm font-medium tracking-wider text-left text-gray-700 uppercase">Nama Aplikasi</th>
-                                    <th class="px-6 py-3 text-sm font-medium tracking-wider text-left text-gray-700 uppercase">Laporan Error</th>
-                                    <th class="px-6 py-3 text-sm font-medium tracking-wider text-left text-gray-700 uppercase">Status</th>
-                                    <th class="px-6 py-3 text-sm font-medium tracking-wider text-left text-gray-700 uppercase">Foto Error</th>
-                                    <th class="px-6 py-3 text-sm font-medium tracking-wider text-left text-gray-700 uppercase">Actions</th>
-                                </tr>
-                            </thead>
-                            <tbody class="bg-white divide-y divide-gray-200">
-                                @foreach($pelapors as $pelapor)
-                                    <tr>
-                                        <td class="px-4 py-2 border">{{ $pelapor->email }}</td>
-                                        <td class="px-4 py-2 border">{{ $pelapor->nohp }}</td>
-                                        <td class="px-4 py-2 border">{{ $pelapor->npelapor }}</td>
-                                        <td class="px-4 py-2 border">{{ $pelapor->pengaduan->naplikasi ?? 'data-tidak-ditemukan' }}</td>
-                                        <td class="px-4 py-2 border">{{ $pelapor->pengaduan->laporan ?? 'data-tidak-ditemukan' }}</td>
-                                        <td class="px-4 py-2 border">{{ $pelapor->pengaduan->status->name ?? 'data-tidak-ditemukan' }}</td>
-                                        <td class="px-4 py-2 border">
-                                            @if ($pelapor->pengaduan && $pelapor->pengaduan->file_foto)
-                                                <!-- Thumbnail yang bisa diklik -->
-                                                <img src="{{ asset('uploads/file_foto/' . $pelapor->pengaduan->file_foto) }}" alt="Foto Pelapor" class="w-20 h-20 cursor-pointer" onclick="document.getElementById('modal-{{ $pelapor->id }}').showModal()">
+                                    <td class="px-4 py-2">{{ $pelapor->email }}</td>
+                                    <td class="px-4 py-2">{{ $pelapor->nohp }}</td>
+                                    <td class="px-4 py-2">{{ $pelapor->npelapor }}</td>
+                                    <td class="px-4 py-2">{{ $pelapor->pengaduan->naplikasi ?? 'data-tidak-ditemukan' }}</td>
+                                    <td class="px-4 py-2">{{ $pelapor->pengaduan->laporan ?? 'data-tidak-ditemukan' }}</td>
+                                    <td class="px-4 py-2">{{ $pelapor->pengaduan->status->name ?? 'data-tidak-ditemukan' }}</td>
+                                    <td class="px-4 py-2">
+                                        @if ($pelapor->pengaduan && $pelapor->pengaduan->file_foto)
+                                            <img src="{{ asset('uploads/file_foto/' . $pelapor->pengaduan->file_foto) }}" alt="Foto Pelapor" class="w-20 h-20 cursor-pointer" onclick="document.getElementById('modal-{{ $pelapor->id }}').showModal()">
 
-                                                <!-- Modal Dialog -->
-                                                <dialog id="modal-{{ $pelapor->id }}" class="modal">
-                                                    <div class="flex justify-center items-center">
-                                                        <!-- Tombol Close di Luar Gambar -->
-                                                        <button class="absolute top-0 right-0 m-4 text-2xl bg-gray-700 text-white px-2 py-1 rounded-full" onclick="document.getElementById('modal-{{ $pelapor->id }}').close()">×</button>
+                                            <dialog id="modal-{{ $pelapor->id }}" class="modal">
+                                                <div class="flex justify-center items-center">
+                                                    <button class="absolute top-0 right-0 m-4 text-2xl bg-gray-700 text-white px-2 py-1 rounded-full" onclick="document.getElementById('modal-{{ $pelapor->id }}').close()">×</button>
+                                                    <img src="{{ asset('uploads/file_foto/' . $pelapor->pengaduan->file_foto) }}" alt="Foto Pelapor Full" class="w-1/2 h-auto">
+                                                </div>
+                                            </dialog>
+                                        @else
+                                            <span>data-tidak-ditemukan</span>
+                                        @endif
+                                    </td>
+                                    <td class="px-4 py-2">
+                                        <div class="inline-flex space-x-2">
+                                            <button onclick="openStatusModal('{{ $pelapor->id }}')" class="px-4 py-2 text-white bg-purpneo rounded-3xl hover:text-neon font-bold hover:scale-105 transition-transform duration-300 w-32">Ubah Status</button>
+                                            <form id="delete-form-{{ $pelapor->id }}" action="{{ route('tambah.destroy', $pelapor->id) }}" method="POST" class="inline">
+                                                @csrf
+                                                @method('DELETE')
+                                                <button type="button" class="px-4 py-2 text-white bg-redneo rounded-3xl shadow-lg hover:text-neon font-bold hover:scale-105 transition-transform duration-300 w-32" onclick="confirmDelete('{{ $pelapor->id }}')">Hapus</button>
+                                            </form>
+                                        </div>
 
-                                                        <!-- Foto ukuran sedang tanpa border -->
-                                                        <img src="{{ asset('uploads/file_foto/' . $pelapor->pengaduan->file_foto) }}" alt="Foto Pelapor Full" class="w-1/2 h-auto">
-                                                    </div>
-                                                </dialog>
-                                            @else
-                                                <span>data-tidak-ditemukan</span>
-                                            @endif
-                                        </td>
-                                        <td class="px-4 py-2 border">
-                                            <div class="inline-flex space-x-2">
-                                                <button onclick="openStatusModal('{{ $pelapor->id }}')" class="px-4 py-2 text-white bg-purpneo rounded-3xl hover:text-neon font-bold hover:scale-105 transition-transform duration-300">Ubah Status</button>
-                                                <form id="delete-form-{{ $pelapor->id }}" action="{{ route('tambah.destroy', $pelapor->id) }}" method="POST" class="inline">
+
+                                        <!-- MODAL DELETE -->
+                                        <div id="deleteModal" class="fixed inset-0 flex items-center justify-center bg-gray-900 bg-opacity-50 hidden">
+                                            <div class="bg-white p-6 rounded-lg shadow-lg">
+                                                <h2 class="text-lg font-bold">Konfirmasi Penghapusan</h2>
+                                                <p>Apakah Anda yakin ingin menghapus data ini?</p>
+                                                <div class="mt-4 flex justify-end space-x-4">
+                                                    <button type="button" onclick="cancelDelete()" class="px-4 py-2 bg-gray-300 text-black rounded-lg hover:bg-gray-400">Batal</button>
+                                                    <button id="confirmDeleteBtn" class="px-4 py-2 bg-red-600 text-white rounded-lg hover:bg-red-700">Hapus</button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <!-- END MODAL DELETE -->
+
+
+                                        <!-- MODAL UBAH STATUS -->
+                                        <div id="statusModal-{{ $pelapor->id }}" class="fixed inset-0 flex items-center justify-center bg-gray-900 bg-opacity-50 hidden">
+                                            <div class="bg-white p-6 rounded-lg shadow-lg">
+                                                <h2 class="text-lg font-bold">Ubah Status untuk {{ $pelapor->npelapor }}</h2>
+                                                <form action="{{ route('pelapor.updateStatus', $pelapor->id) }}" method="POST">
                                                     @csrf
-                                                    @method('DELETE')
-                                                    <button type="button" class="px-6 py-6 text-white bg-redneo rounded-3xl shadow-lg hover:text-neon font-bold hover:scale-105 transition-transform duration-300" onclick="confirmDelete('{{ $pelapor->id }}')">Hapus</button>
+                                                    <div class="mt-4">
+                                                        <label for="status" class="block text-sm font-medium">Pilih Status:</label>
+                                                        <select name="status" id="status" class="mt-1 block w-full border-gray-300 rounded-md shadow-sm font-[Outfit]">
+                                                        @foreach($statues as $sts)
+                                                            <option value="{{ $sts->id }}" {{ $pelapor->pengaduan && $pelapor->pengaduan->status_id == $sts->id ? 'selected' : '' }}>
+                                                                {{ $sts->name }}
+                                                            </option>
+                                                        @endforeach
+                                                        </select>
+                                                    </div>
+                                                    <div class="mt-4 flex justify-end space-x-4">
+                                                        <button type="button" onclick="closeStatusModal('{{ $pelapor->id }}')" class="px-4 py-2 bg-gray-300 text-black rounded-lg hover:bg-gray-400">Batal</button>
+                                                        <button type="submit" class="px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700">Update Status</button>
+                                                    </div>
                                                 </form>
                                             </div>
-                                            <!-- MODAL DELETE -->
-                                            <div id="deleteModal" class="fixed inset-0 flex items-center justify-center bg-gray-900 bg-opacity-50 hidden">
-                                                <div class="bg-white p-6 rounded-lg shadow-lg">
-                                                    <h2 class="text-lg font-bold">Konfirmasi Penghapusan</h2>
-                                                    <p>Apakah Anda yakin ingin menghapus data ini?</p>
-                                                    <div class="mt-4 flex justify-end space-x-4">
-                                                        <button type="button" onclick="cancelDelete()" class="px-4 py-2 bg-gray-300 text-black rounded-lg hover:bg-gray-400">Batal</button>
-                                                        <button id="confirmDeleteBtn" class="px-4 py-2 bg-red-600 text-white rounded-lg hover:bg-red-700">Hapus</button>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <!-- MODAL UBAH STATUS -->
-                                            <div id="statusModal-{{ $pelapor->id }}" class="fixed inset-0 flex items-center justify-center bg-gray-900 bg-opacity-50 hidden">
-                                                <div class="bg-white p-6 rounded-lg shadow-lg">
-                                                    <h2 class="text-lg font-bold">Ubah Status untuk {{ $pelapor->npelapor }}</h2>
-                                                    <form action="{{ route('pelapor.updateStatus', $pelapor->id) }}" method="POST">
-                                                        @csrf
-                                                        <div class="mt-4">
-                                                            <label for="status" class="block text-sm font-medium">Pilih Status:</label>
-                                                            <select name="status" id="status" class="mt-1 block w-full border-gray-300 rounded-md shadow-sm font-[Outfit]">
-                                                            @foreach($statues as $sts)
-                                                                <option value="{{ $sts->id }}" {{ $pelapor->pengaduan && $pelapor->pengaduan->status_id == $sts->id ? 'selected' : '' }}>
-                                                                    {{ $sts->name }}
-                                                                </option>
-                                                            @endforeach
-                                                            </select>
-                                                        </div>
-                                                        <div class="mt-4 flex justify-end space-x-4">
-                                                            <button type="button" onclick="closeStatusModal('{{ $pelapor->id }}')" class="px-4 py-2 bg-gray-300 text-black rounded-lg hover:bg-gray-400">Batal</button>
-                                                            <button type="submit" class="px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700">Update Status</button>
-                                                        </div>
-                                                    </form>
-                                                </div>
-                                            </div>
-                                        </td>
-                                    </tr>
+                                        </div>
+                                        <!-- End Untuk Modal Update -->
+                                    </td>
+                                </tr>
                                 @endforeach
                             </tbody>
                         </table>
+                    </div>
                 </div>
             </div>
         </div>
+        <!-- END TABEL PELAPORAN  -->
     </div>
-</div>
 
-<script>
+    <script>
     //Code untuk Data Tables
     $(document).ready(function() {
         $('#pelaporTable').DataTable({
--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\PelaporController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\JenisController;
-use App\Http\Controllers\StatusController; // Tambahkan controller Status
+use App\Http\Controllers\StatusController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth')->group(function () {
@@ -12,24 +12,24 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-    // Dashboard route
+
+    // View  Routes
     Route::get('/dashboard', [PelaporController::class, 'index'])->middleware(['verified'])->name('dashboard');
     Route::get('/pelapor', [PelaporController::class, 'pelapor'])->middleware(['verified'])->name('pelapor');
     Route::get('/aduan', [PelaporController::class, 'aduan'])->middleware(['verified'])->name('aduan');
-
-    // Pelapor routes
-    Route::delete('/tambah/{id}', [PelaporController::class, 'destroy'])->name('tambah.destroy');
-
-    // Jenis routes
     Route::get('/jenis', [JenisController::class, 'index'])->name('jenis.index');
+    Route::get('/status', [StatusController::class, 'index'])->name('status.index');
+
+
+    // Jenis routes Crud
     Route::get('/jenis/create', [JenisController::class, 'create'])->name('jenis.create');
     Route::post('/jenis', [JenisController::class, 'store'])->name('jenis.store');
     Route::get('/jenis/{id}/edit', [JenisController::class, 'edit'])->name('jenis.edit');
     Route::put('/jenis/{id}', [JenisController::class, 'update'])->name('jenis.update');
     Route::delete('/jenis/{id}', [JenisController::class, 'destroy'])->name('jenis.destroy');
 
-    // Status routes
-    Route::get('/status', [StatusController::class, 'index'])->name('status.index');
+
+    // Status routes Crud
     Route::get('/status/create', [StatusController::class, 'create'])->name('status.create');
     Route::post('/status', [StatusController::class, 'store'])->name('status.store');
     Route::delete('/status/{id}', [StatusController::class, 'destroy'])->name('status.destroy');
@@ -37,21 +37,24 @@ Route::middleware('auth')->group(function () {
     Route::put('/status/{id}', [StatusController::class, 'update'])->name('status.update');
 });
 
-// Route untuk menambah data pelapor
-Route::get('/', [PelaporController::class, 'create'])->name('tambah.create');
-Route::post('/tambah/data', [PelaporController::class, 'store'])->name('tambah.store');
-Route::post('/pelapor/{id}/status', [PelaporController::class, 'updateStatus'])->name('pelapor.updateStatus');
+
+    // Routes untuk Form Layanan Pemerintahan
+    Route::get('/', [PelaporController::class, 'create'])->name('tambah.create');
+    Route::post('/tambah/data', [PelaporController::class, 'store'])->name('tambah.store');
+    Route::post('/pelapor/{id}/status', [PelaporController::class, 'updateStatus'])->name('pelapor.updateStatus');
+    Route::delete('/tambah/{id}', [PelaporController::class, 'destroy'])->name('tambah.destroy');
 
 
-// Route untuk kode antrian
-Route::get('/kodeantrian/{kode}', function ($kode) {
-    return view('kodeantrian', ['kode' => $kode]);
-})->name('kodeantrian');
+    // Route untuk kode antrian
+    Route::get('/kodeantrian/{kode}', function ($kode) {
+        return view('kodeantrian', ['kode' => $kode]);
+    })->name('kodeantrian');
+    //Route Cek Status
+    Route::get('/ceksts', function()
+    {
+        return view('ceksts');
+    });
 
-Route::get('/ceksts', function()
-{
-    return view('ceksts');
-});
-// Route::post('/ceksts', [PelaporController::class, 'cekStatus'])->name('cekStatus');
-// Menghubungkan file otentikasi Laravel
+
+
 require __DIR__ . '/auth.php';
